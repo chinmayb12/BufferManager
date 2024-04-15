a8de6eb8181d9865a01caa031cdfa47bafc929c4
Chinmay <starscnb06@gmail.com>
2024-04-15 14:00:57 +0530

    main.hpp

---
+++ main.hpp
@@ -0,0 +1,28 @@
+#ifndef __SIMULATE__
+#define __SIMULATE__
+
+#include "bufferManager.hpp"
+using namespace std;    
+
+enum policy{
+    LRU = 1,
+    MRU,
+    CLOCK
+};
+
+#define PAGE_SIZE 4096
+
+// process a select from query
+
+class simulate{
+private:
+    baseBufferManager* bufferManager;
+
+public:
+    simulate(int numFrames, policy PolicyType);
+    void processSelectQuery(FILE *file_pointer, int col1, string value,int comp);
+    void processJoinQuery(FILE *file_pointer1, FILE *file_pointer2, int col1, int col2);
+};
+
+
+#endif
